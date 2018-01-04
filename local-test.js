4da7ebfe649dbca13f97f04daec7496fec68cc2b
PatyYe <patyye@outlook.com>
2018-01-04 18:11:09 +0100

    Delete local-test.js

--- local-test.js
+++
@@ -1,6 +0,0 @@
-var filename = `./servers/pubg/222979003869429761.json`;
-var props = require(filename);
-
-for(var attributename in props["ranking"]){
-    console.log(attributename+": "+props["ranking"][attributename]);
-}
